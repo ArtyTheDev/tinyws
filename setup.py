b36b45a6d83cc85be35081b9958c4f57a79f237e
Arty <artythedev@gmail.com>
2022-12-22 04:01:15 +0300

    feat(add): setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='tinyws',
+    version='0.1',
+    description='A tiny package for making websocket server.',
+    author='Arty',
+    author_email='artythedev@gmail.com',
+    url='https://github.com/ArtyTheDev/tinyws',
+    packages=["tinyws"],
+    python_requires=">=3.6",
+    platforms="any",
+)
